6f2c0d99cd786b8ff894a9a8bdd8ff9c47ee6752
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-20 11:26:40 -0700

    Add google calendar get request

--- src/contacts/contacts-request.js
+++ src/contacts/contacts-request.js
@@ -4,7 +4,7 @@ const URL = 'https://better-friend.herokuapp.com';
 // const URL = 'http://localhost:7890';
 
 const getContacts = async (token) => {
-  const response = await request.get(`${URL}/api/contacts`).set('Authorization', token); // will need an update when /api route added to include .set
+  const response = await request.get(`${URL}/api/contacts`).set('Authorization', token);
 
   return response.body;
 };
