d622965da8ed04ef0ecb292760984eb3e386a409
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-20 15:22:23 -0700

    Add calendar get and post requests

--- src/details-page/calendar-get-request.js
+++ src/details-page/calendar-get-request.js
@@ -1,13 +1,10 @@
-// google auth token
-// api key
-
 import request from 'superagent';
 
 const URL = 'https://better-friend.herokuapp.com';
 // const URL = 'http://localhost:7890';
 
 const getCalendarEvents = async (token, oauth) => {
-  const response = await request.get(`${URL}/api/calendar/${oauth}`).set('Authorization', token);
+  const response = await request.get(`${URL}/api/calendar`).set({ 'Authorization': token, 'Oauth': oauth });
 
   return response.body;
 };
