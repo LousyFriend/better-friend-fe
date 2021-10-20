22805129d7169f0a1001a3e7c77d8fd1b55625a8
Michael Rider <mikepdxrider@gmail.com>
2021-10-20 16:22:07 -0700

    adds delete button to comments

--- src/details-page/components/CommentItem.js
+++ src/details-page/components/CommentItem.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
+import deleteComment from '../delete-comment-utils.js';
 
 export default class CommentItem extends Component {
-    // Still needs:
-    //  - Timestamp?
-    //  - A delete/edit button in li jsx which makes a DELETE/PUT request.
-    //      - Will require token from props. 
-    //      - Will require retrieveComments function from comment section too.
+    
     
   render() {
     //   Grab comment data from props to create list item.
-    const { commentData } = this.props;
+    const { commentData, token, key } = this.props;
     return (
       <li>
         {/* <p>{commentData.timestamp}</p> */}
         <p>{commentData.comment}</p>
+        <button onClick={() => deleteComment(token, key)}></button>
+
       </li>
     );
   }
