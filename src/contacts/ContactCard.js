93b36df164d4e518625c2a14e24f0f95828d8806
DanielRGilles <danryangilles@gmail.com>
2021-10-21 20:37:59 -0700

    styling

--- src/contacts/ContactCard.js
+++ src/contacts/ContactCard.js
@@ -9,23 +9,23 @@ export default class ContactCard extends Component {
       <Link to={`/details/${id}`} style={{ margin: '30px' }}>
         {
           <div class='w-full bg-white rounded-lg shadow-xl overflow-hidden flex flex-col md:flex-row lg:justify-center h-full'>
-            <div class='w-screen md:w-1/2 align-left p-4 flex-1 md:p-10 m-0 grid grid-cols-1 h-auto justify-start'>
+            <div class='w-screen md:w-1/2 align-left p-4 flex-1 md:p-10 m-0 grid grid-cols-1 h-auto justify-start relative'>
               {
                 image_url === null || image_url === undefined || image_url === ''
-                  // ? <img src={`${process.env.PUBLIC_URL}/assets/blank-avatar.jpg`} alt='blank avatar' width='300px' height='200px'/>
-                  ? <img class='object-left rounded-lg object-cover w-60 h-80% ' src='blank-avatar.jpg' alt='blank avatar'/>
-                  : <img class='object-center rounded-lg object-cover w-60 h-full' src={ image_url } alt='blank avatar'></img>
+                  ? <img class='object-left rounded-lg object-cover w-60 h-80% absolute left-2 top-2 ' src='blank-avatar.jpg' alt='blank avatar'/>
+                  : <img class='object-center rounded-lg object-cover w-60 h-full absolute left-2 top-2' src={ image_url } alt='blank avatar'></img>
               }
-            </div>
-            <section class='flex flex-row p-4'>
-              <div class='text-right'>{ name }</div>
-              <div class='text-right'>{ job_title }</div>
-            </section>
-            <footer class='flex justify-self-end'>
-              <div>{ gmail }</div>
-              <div>{ phone }</div>
-              <div>{ linked_in }</div>
-            </footer> 
+            
+              <div class='relative text-xs'>
+                <section class='flex flex-col p-4 absolute right-8'>
+                  <div class='text-right'>{ name }</div>
+                  <div class='text-right'>{ job_title }</div>
+                </section>
+                <footer class='flex flex-row absolute right-1 bottom-0'>
+                  <div>{ gmail }gmail </div>
+                  <div>{ phone }phone number </div>
+                  <div>{ linked_in }linkedin </div>
+                </footer> </div></div>
           </div>
         }
         {/* name, job title last contacted email phone linkedin */}
