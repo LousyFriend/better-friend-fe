db962c48744214567d919b6cf0f4e7a984a36295
Zack Lyon <zack.j.lyon@gmail.com>
2021-10-21 20:09:17 -0700

    Working put and post routes

--- src/calendar-page/calendar-put-request.js
+++ src/calendar-page/calendar-put-request.js
@@ -3,9 +3,8 @@ import request from 'superagent';
 // const URL = 'https://better-friend.herokuapp.com';
 const URL = 'http://localhost:7890';
 
-const putCalendarEvent = async (token, oauth, contactId, body) => {
-  //body needs event_id and next_date
-  const response = await request.put(`${URL}/api/calendar/${contactId}`).send(body).set({ 'Authorization': token, 'Oauth': oauth });
+const putCalendarEvent = async (token, oauth, eventId, body) => {
+  const response = await request.put(`${URL}/api/calendar/${eventId}`).send(body).set({ 'Authorization': token, 'Oauth': oauth });
   
   return response.body;
 };
